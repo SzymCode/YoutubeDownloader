bc8b005b687c498ef3948cca25ee6b805cfd7854
Pythadela <s.radomski19@gmail.com>
2022-11-12 21:48:11 +0100

    Download video

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter import *
+from pytube import YouTube
+
 
 window = Tk()
 window.geometry('300x120')
@@ -6,4 +8,17 @@ window.resizable(False, False)
 window.configure(bg='white')
 window.title("YT downloader")
 window.iconbitmap("images\\icon.ico")
+
+Label(window, text='Youtube Video Downloader', font='karla 16 bold', bg='white').pack()
+link = StringVar()
+Entry(window, width=40, textvariable=link).place(x=32, y=32)
+
+
+def Downloader():
+    url = YouTube(str(link.get()))
+    video = url.streams.first()
+    video.download()
+
+
+Button(window, text='DOWNLOAD', font='karla 15 bold', bg='#7bb2ed', padx=2, command=Downloader).place(x=80, y=62)
 window.mainloop()
