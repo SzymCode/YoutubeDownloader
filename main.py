f5ee46ec405cc7f32f260f4edba03ba723bd4b95
Pythadela <s.radomski19@gmail.com>
2022-11-12 21:45:54 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+window = Tk()
+window.geometry('300x120')
+window.resizable(False, False)
+window.configure(bg='white')
+window.title("YT downloader")
+window.iconbitmap("images\\icon.ico")
+window.mainloop()
